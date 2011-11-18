bf649e269008055c5d859120b5eef61543093711
Андрей Малышев <anazapta@gmail.com>
2011-11-18 16:30:52 +0400

    Https in production

--- lib/irwi/extensions/controllers/wiki_page_attachments.rb
+++ lib/irwi/extensions/controllers/wiki_page_attachments.rb
@@ -3,13 +3,13 @@ module Irwi::Extensions::Controllers::WikiPageAttachments
   def add_attachment
     attachment = page_attachment_class.new(params[:wiki_page_attachment])
     attachment.save
-    redirect_to url_for(:path => attachment.page.path, :action => :edit)
+    redirect_to url_for(:path => attachment.page.path, :action => :edit, :protocol=>(Rails.env=="production" ? "https://" : "http://"))
   end
 
   def remove_attachment
     attachment = page_attachment_class.find(params[:attachment_id])
     attachment.destroy
-    redirect_to url_for(:path => attachment.page.path, :action => :edit)
+    redirect_to url_for(:path => attachment.page.path, :action => :edit, :protocol => (Rails.env=="production" ? "https://" : "http://"))
   end
 
   protected
